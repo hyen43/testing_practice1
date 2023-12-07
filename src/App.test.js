a9d2df64f0b8e9024f22b378744c58bd75503e1f
Nahyun Lee <lnhyen43@gmail.com>
2023-12-07 14:53:56 +0900

    TEST: test 정규표현식에서 문자열로 변경

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
+  // Run render method(JSX에 관련한 인수)
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Learn React");
   expect(linkElement).toBeInTheDocument();
 });
