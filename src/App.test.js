de1a45d99bbcf0b4bc5ae68b9750547fcdf5e680
Nahyun Lee <lnhyen43@gmail.com>
2023-12-07 17:14:39 +0900

    TEST: 초기 상태에 대한 TDD 진행

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  // Run render method(JSX에 관련한 인수)
+test("button has correct initial color", () => {
+  // 렌더링할 컴포넌트 작성
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+
+  // 살펴보려는 요소: 버튼 역할과 초기 텍스트가 아래와 같은 요소를 찾음.
+  const colorBtn = screen.getByRole("button", { name: /change to blue/i });
+
+  // 배경색이 빨간색인 버튼을 기대함(테스트 내용)
+  // 문자열이나 객체 모두 가능하다.
+  expect(colorBtn).toHaveStyle({
+    backgroundColor: "red",
+  });
 });
+
+test("button turns blue when clicked", () => {});
